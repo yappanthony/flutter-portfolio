a42e874a2d467c9554448f5be79bfc2f67957c48
Anthony Yap <yappanthony22@gmail.com>
2024-10-23 13:10:22 +0800

    added highlight to greeting header

--- lib/widgets/greeting_text.dart
+++ lib/widgets/greeting_text.dart
@@ -13,33 +13,62 @@ class GreetingText extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
+    bool isDesktop(BuildContext context) => 
+      MediaQuery.sizeOf(context).width >= 1000;
+
+    bool isMobile(BuildContext context) =>
+      MediaQuery.sizeOf(context).width < 1000;
+    return Stack(
       children: [
-        Text(
-          "👋 Hey there,",
-          style: TextStyle(
-            fontSize: headerFontSize,
-            fontWeight: FontWeight.bold
+        if(isDesktop(context))
+          Positioned(
+            top: 115,
+            child: Container(
+              height: 20,
+              width: 400,
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(colors: [Color.fromARGB(255, 250, 169, 47), Color.fromARGB(255, 255, 193, 101)]),
+              )
+            ),
           ),
-        ),
-        Text(
-          "It's Anthony Yap.",
-          style: TextStyle(
-            fontSize: headerFontSize,
-            fontWeight: FontWeight.bold
+        Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          if(isMobile(context))
+            Text(
+              "👋 Hey there, It's Anthony Yap.",
+              style: TextStyle(
+                fontSize: headerFontSize,
+                fontWeight: FontWeight.bold
+              ),
+            ),
+          if(isDesktop(context))
+            Text(
+              "👋 Hey there,",
+              style: TextStyle(
+                fontSize: headerFontSize,
+                fontWeight: FontWeight.bold
+              ),
+            ),
+          if(isDesktop(context))
+            Text(
+              "It's Anthony Yap.",
+              overflow: TextOverflow.visible,
+              style: TextStyle(
+                fontSize: headerFontSize,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          Text(
+            "Welcome to my developer profile! I'm Anthony Yap, a passionate software developer with four years of coding experience. I specialize in backend development and I'm always eager to learn new things and take on challenging projects",
+            style: TextStyle(
+              fontWeight: FontWeight.w500,
+              fontSize: textFontSize,
+              color: const Color.fromARGB(255, 143, 143, 143),
+            ),
           ),
-        ),
-        const SizedBox(height: 20,),
-        Text(
-          "Welcome to my developer profile! I'm Anthony Yap, a passionate software developer with four years of coding experience. I specialize in backend development and I'm always eager to learn new things and take on challenging projects",
-          style: TextStyle(
-            fontWeight: FontWeight.w500,
-            fontSize: textFontSize,
-            color: const Color.fromARGB(255, 143, 143, 143),
-          ),
-        ),
-      ],
+        ],
+      )],
     );
   }
 }
